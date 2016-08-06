a1e67e3f25caf63296da39c6b39259d2568b5dbc
VitalikSt <vitalik.sts@gmail.com>
2016-08-07 01:03:35 +0300

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
-/*===========================*/
-$(document).ready(function(){
-
-
-});
-/************//**************/
+/*==========================================*/
+    $(document).ready(function(){
+      $(".header-top").removeClass("default");
+      $(window).scroll(function(){
+        if ($(this).scrollTop() > 20) {
+          $(".header-top").addClass("default").fadeIn('fast');
+        } else {
+          $(".header-top").removeClass("default").fadeIn('fast');
+        };
+      });
+    });
